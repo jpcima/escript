8f628686c7ff0ce31309ea86c26c9cbb90a76a98
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-07-17 12:13:22 +0200

    Add slider example

--- examples/script/resources/script.tcl
+++ examples/script/resources/script.tcl
@@ -1,4 +1,5 @@
 hsize -id main 800 \
     [vtile \
          [button -size 2.0 -icon left -icon_pos right -body_color #337700 {Hello World}] \
-         [top_margin 20 [button Other]]]
+         [top_margin 20 [button Other]] \
+         [slider [basic_thumb 25] [basic_track 100]]]
