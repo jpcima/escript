10bdcb018d36d319ef81e7415357e92734ed6b59
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-07-17 13:52:19 +0200

    Example update

--- examples/script/resources/script.tcl
+++ examples/script/resources/script.tcl
@@ -2,4 +2,4 @@ hsize -id main 800 \
     [vtile \
          [button -size 2.0 -icon left -icon_pos right -body_color #337700 {Hello World}] \
          [top_margin 20 [button Other]] \
-         [slider [basic_thumb 25] [slider_marks 20 [slider_labels 20 16.0 {L1 L2 L3} [basic_track 30]]]]]
+         [slider [basic_thumb 25] [slider_marks 20 [slider_labels 20 16.0 {L1 L2 L3} [basic_track 5]]]]]
